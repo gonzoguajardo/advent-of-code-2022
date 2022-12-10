74cece6ce5af8b153cd544125569ff8793ed0032
Gonzo Guajardo <gonzo.guajardo@gmail.com>
2022-12-09 19:38:13 -0600

    day 7 part 2

---
+++ day7-part2/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	currentDirectoryPath := ""
+	currentDirectorySlice := make([]string, 0)
+	directoryMap := make(map[string][]string)
+	directorySizeMap := make(map[string]int)
+	// var unresolvedDirectory bool
+	var previousLs bool
+
+	count := 0
+
+	for scanner.Scan() {
+		currentString := scanner.Text()
+		// log.Println(currentString)
+		isCommand := strings.Contains(currentString, "$")
+
+		if isCommand {
+			if previousLs {
+				directoryMap[currentDirectoryPath] = currentDirectorySlice
+				ok, currentDirectorySize := checkDirectory(currentDirectoryPath, currentDirectorySlice, directorySizeMap)
+				if ok {
+					directorySizeMap[currentDirectoryPath] = currentDirectorySize
+				}
+				previousLs = false
+			}
+			commandSplit := strings.Split(currentString, "$ ")
+			command := commandSplit[1]
+			cd := strings.Contains(command, "cd ")
+			if cd {
+				cdSplit := strings.Split(command, "cd ")
+				directory := cdSplit[1]
+				isUpDirectory := strings.Contains(directory, "..")
+				if isUpDirectory {
+					removeLast := currentDirectoryPath[:len(currentDirectoryPath) - 1]
+					lastIndex := strings.LastIndex(removeLast, "/")
+					if lastIndex != 0 {
+						newDirectoryPath := currentDirectoryPath[:lastIndex+1]
+						currentDirectoryPath = newDirectoryPath
+					} else {
+						currentDirectoryPath = "/"
+					}
+
+					ok, currentDirectorySize := checkDirectory(currentDirectoryPath, directoryMap[currentDirectoryPath], directorySizeMap)
+					if ok {
+						directorySizeMap[currentDirectoryPath] = currentDirectorySize
+					}
+
+				} else {
+					if len(currentDirectoryPath) != 0 {
+					}
+					if currentDirectoryPath == "" {
+						currentDirectoryPath += directory
+					} else {
+						currentDirectoryPath += directory + "/"
+					}
+					currentDirectorySlice = make([]string, 0)
+
+				}
+			}
+		} else {
+			previousLs = true
+			currentDirectorySlice = append(currentDirectorySlice, currentString)
+		}
+		count++
+	}
+	directoryMap[currentDirectoryPath] = currentDirectorySlice
+
+	totalSize := 0
+	for len(directoryMap) > 0 {
+		for k, v := range(directoryMap){
+			directoryFound := true
+			if value, ok := directorySizeMap[k]; ok {
+				if value < 100000 {
+					totalSize += value
+				} else {
+				}
+			} else {
+				ok, currentDirectorySize := checkDirectory(k, v, directorySizeMap)
+				if ok {
+					directorySizeMap[k] = currentDirectorySize
+					if currentDirectorySize < 100000 {
+						totalSize += currentDirectorySize
+					}
+				} else {
+					directoryFound = false
+				}
+			}
+			if directoryFound {
+				delete(directoryMap, k)
+			}
+		}
+
+	}
+
+
+	totalSize = directorySizeMap["/"]
+	sizeLeft := 70000000 - totalSize
+	minThreshold := 30000000 - sizeLeft
+	minDirectorySize := math.MaxInt32
+
+	for _, v := range(directorySizeMap) {
+		if v >= minThreshold {
+			if v < minDirectorySize {
+				minDirectorySize = v
+			}
+		}
+	}
+
+	log.Println(minDirectorySize)
+
+}
+
+func checkDirectory(currentDirectory string, directorySlice []string, directorySizeMap map[string]int) (bool, int){
+	currentDirectorySize := 0
+	for _, currentString := range(directorySlice) {
+		isDir := strings.Contains(currentString, "dir ")
+		spaceSplit := strings.Split(currentString, " ")
+		if isDir {
+			directoryToCheck := currentDirectory + spaceSplit[1] + "/"
+			if value, ok := directorySizeMap[directoryToCheck]; ok {
+				currentDirectorySize += value
+			} else {
+				return false, 0
+			}
+		} else {
+			currentSize , _ := strconv.Atoi(spaceSplit[0])
+			currentDirectorySize += currentSize
+		}
+	}
+	return true, currentDirectorySize
+}
